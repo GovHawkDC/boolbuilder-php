5adccfb1ba2caf6e58dbec8ea4495c370c781bf7
doubleswirve <saxon.stiller@gmail.com>
2018-03-29 16:38:32 -0400

    string split test

--- src/esTest.php
+++ src/esTest.php
@@ -5,8 +5,13 @@ use PHPUnit\Framework\TestCase;
 
 final class EsTest extends TestCase
 {
-    public function testArrayArgIsSelf()
+    public function testArrayArgToGetArrayValueIsSelf()
     {
         $this->assertEquals(\Boolbuilder\ES\getArrayValue([1, 2]), [1, 2]);
+    }
+
+    public function testStringArgToGetArrayValueIsArray()
+    {
+        $this->assertEquals(\Boolbuilder\ES\getArrayValue('1, 2'), ['1', '2']);
     }
 }
