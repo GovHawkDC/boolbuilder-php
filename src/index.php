e2acadf853104c3d8994aaf8e9b52f0de152516d
doubleswirve <saxon.stiller@gmail.com>
2018-03-30 11:48:05 -0400

    rough attempty to port over main transform func

--- src/index.php
+++ src/index.php
@@ -3,7 +3,36 @@ namespace Boolbuilder;
 
 function transform($group, $filters = [])
 {
+    if (!$group) {
+        return [];
+    }
 
+    $QB = isset($group['QB']) ? $group['QB'] : '';
+    $rules = isset($group['$rules']) ? $group['$rules'] : [];
+
+    if (count($rules) < 1) {
+        return [];
+    }
+
+    $postFilterUserFuncName = __NAMESPACE__ . '\\transformGroupPostFilter';
+
+    if (isset($filters[$QB])) {
+        $userProvidedFilter = $filters[$QB];
+        return $userProvidedFilter(
+            $group,
+            $rules,
+            $filters,
+            $postFilterUserFuncName
+        );
+    }
+
+    return call_user_func(
+        __NAMESPACE__ . '\\transformGroupDefaultFilter',
+        $group,
+        $rules,
+        $filters,
+        $postFilterUserFuncName
+    );
 }
 
 function transformGroupPostFilter($group, $rules, $filters)
