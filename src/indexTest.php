231bb17b0c0bf0f09563fe18de126e8d1e75576e
doubleswirve <saxon.stiller@gmail.com>
2018-06-19 10:44:19 -0700

    write failing test for only fields inverse filter

--- src/indexTest.php
+++ src/indexTest.php
@@ -466,4 +466,104 @@ final class IndexTest extends TestCase
 
         $this->assertEquals($result, $expected);
     }
+
+    public function testQBData8()
+    {
+        $QBdata = [
+            'condition' => 'AND',
+            'rules' => [
+                [
+                    'id' => 'name',
+                    'field' => 'name',
+                    'type' => 'string',
+                    'input' => 'text',
+                    'operator' => 'contains',
+                    'value' => '123'
+                ],
+                [
+                    'QB' => 'book',
+                    'condition' => 'or',
+                    'rules' => [
+                        [
+                            'id' => 'misc',
+                            'field' => 'misc',
+                            'type' => 'string',
+                            'input' => 'text',
+                            'operator' => 'is_null',
+                            'value' => null
+                        ],
+                        [
+                            'id' => 'type',
+                            'field' => 'type',
+                            'type' => 'string',
+                            'input' => 'checkbox',
+                            'operator' => 'in',
+                            'value' => ['book']
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $filters = [
+            'book' => function (
+                $group,
+                $rules,
+                $filters,
+                $options,
+                $postFilterUserFuncName
+            ) {
+                return [
+                    'must' => [
+                        ['terms' => ['_type' => ['book']]],
+                        [
+                            'bool' => call_user_func(
+                                $postFilterUserFuncName,
+                                $group,
+                                $rules,
+                                $filters,
+                                $options
+                            )
+                        ]
+                    ]
+                ];
+            }
+        ];
+
+        $options = ['onlyFields' => ['misc']];
+
+        $result = Boolbuilder\transform($QBdata, $filters, $options);
+
+        $expected = [
+            'bool' => [
+                'must' => [
+                    [
+                        'bool' => [
+                            'must' => [
+                                [
+                                    'bool' => [
+                                        'should' => [
+                                            [
+                                                'bool' => [
+                                                    'must_not' => [
+                                                        [
+                                                            'exists' => [
+                                                                'field' => 'misc'
+                                                            ]
+                                                        ]
+                                                    ]
+                                                ]
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $this->assertEquals($result, $expected);
+    }
 }
