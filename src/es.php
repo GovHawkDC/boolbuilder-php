202ed6bf892988fb2b078e2383187aef56dd732c
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-09-13 13:25:35 -0700

    more refactoring to move all of the es syntax outta the entry file and into es helpers

--- src/es.php
+++ src/es.php
@@ -9,17 +9,39 @@ function getClause($group, $rule)
         case 'OR':
             return 'should';
         case 'AND':
-            if (isset($rule['operator']) && isNegativeOperator($rule['operator'])) {
+            // Kinda confusing, but, w/ nesting, sometimes $rule can be a "group", so we
+            // need to make sure $rule is a "rule" by testing for the absence of nested
+            // rules (as a rule terminates; it doesn't have nested sub-rules)
+            if (!isset($rule['rules']) && isNegativeOperator($rule)) {
                 return 'must_not';
             }
             return 'must';
         default:
-            $e = sprintf('Unknown condition "%s"', strval($rule['condition']));
+            $e = sprintf('Unknown condition "%s"', strval($condition));
             throw new \Exception($e);
     }
 }
 
-function getFragment($rule)
+function getFragment($group, $rule)
+{
+    $fragment = getFragmentHelper($group, $rule);
+    // this is a corner case, when we have an "or" group and a negative operator, we
+    // express this with a nested bool query and "must_not"
+    $condition = isset($group['condition']) ? $group['condition'] : 'AND';
+    $condition = strtoupper($condition);
+    if ($condition === 'OR' && isNegativeOperator($rule)) {
+        return [
+            'bool' => [
+                'must_not' => [
+                    $fragment
+                ]
+            ]
+        ];
+    }
+    return $fragment;
+}
+
+function getFragmentHelper($group, $rule)
 {
     if (isWildcardesque($rule)) {
         if (is_string($rule['value'])) {
@@ -132,16 +154,14 @@ function getFragment($rule)
     }
 }
 
-function isNegativeOperator($operator)
+function getQuery($fragments)
 {
-    switch ($operator) {
-        case 'is_null':
-        case 'not_equal':
-        case 'not_in':
-            return true;
-        default:
-            return false;
+    if (empty($fragments)) {
+        return [];
     }
+    return [
+        'bool' => $fragments
+    ];
 }
 
 function isBoostesque($rule)
@@ -156,6 +176,16 @@ function isBoostesque($rule)
     return false;
 }
 
+function isNegativeOperator($rule)
+{
+    $negativeOps = [
+        'is_null',
+        'not_equal',
+        'not_in'
+    ];
+    return in_array($rule['operator'], $negativeOps, true);
+}
+
 function isWildcardesque($rule)
 {
     if ($rule['type'] !== 'string') {
@@ -164,7 +194,12 @@ function isWildcardesque($rule)
     // The existence operators 'is_not_null' and 'is_null' do not require checking of the
     // value string, so we bypass wildcard pattern check... Proximity is purposely
     // avoided so we don't create a false positive where "slop" is intended
-    if (in_array($rule['operator'], ['is_not_null', 'is_null', 'proximity'], true)) {
+    $skipOps = [
+        'is_not_null',
+        'is_null',
+        'proximity'
+    ];
+    if (in_array($rule['operator'], $skipOps, true)) {
         return false;
     }
     // Value is a string (e.g., via single text field) and its type is intended to be a string
