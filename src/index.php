90c6fca190f8d2cf6ef6162021c2442c7190426d
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-09-16 17:13:30 -0700

    unfriendly commit; but almost there w/ the big changes

--- src/index.php
+++ src/index.php
@@ -3,82 +3,107 @@ namespace GovHawkDC\Boolbuilder;
 
 use GovHawkDC\Boolbuilder\ES;
 
-function transform($group, $options = [])
+const DEFAULT_QB = 'QBGroup';
+
+function handleGroup($group, $options, $context)
 {
-    return transformGroup($group, $options);
+    // Setting to default to simplify other checks
+    if (!isset($group['QB'])) {
+        $group['QB'] = $context;
+    }
+    // We don't want to allow user funcs to be applied to the default "QB" or to
+    // type contexts that have already been created...
+    if (in_array($group['QB'], [DEFAULT_QB, $context], true)) {
+        return $group;
+    }
+    // We do not allow for nested processing of custom types
+    if ($context !== DEFAULT_QB) {
+        throw new \Exception('Unable to process nested group of different custom type');
+    }
+    // Apply user func to $group if available
+    if (isset($options['typeFuncMap'][$group['QB']])) {
+        $userFunc = $options['typeFuncMap'][$group['QB']];
+        return $userFunc($group, $options);
+    }
+    return $group;
 }
 
-function transformGroup($group, $options)
+function handleRule($group, $rule, $options)
 {
-    $QB = isset($group['QB']) ? $group['QB'] : '';
-    if (empty($group['rules'])) {
-        return [];
-    }
-    // Allow user functions to handle specific data types (e.g., change the default output)
-    // while processing
-    if (isset($options['typeFuncMap'][$QB])) {
-        $userFunc = $options['typeFuncMap'][$QB];
-        $nextFunc = __NAMESPACE__ . '\\transformRules';
-        return $userFunc($group, $group['rules'], $options, $nextFunc);
-    }
-    return transformRules($group, $group['rules'], $options);
-}
-
-function transformRules($group, $rules, $options)
-{
-    $ts = [];
-    foreach ($rules as $rule) {
-        $fragment = transformRule($group, $rule, $options);
-        if (empty($fragment)) {
-            continue;
-        }
-        $clause = ES\getClause($group, $rule);
-        if (!isset($ts[$clause])) {
-            $ts[$clause] = [];
-        }
-        $ts[$clause][] = $fragment;
-    }
-    return ES\getQuery($ts);
-}
-
-function transformRule($group, $rule, $options)
-{
+    // We do not want to attempt to process a nested "group" as a "rule"
     if (isset($rule['rules'])) {
-        return transformGroup($rule, $options);
+        return $rule;
     }
-
-    if (isRuleExcluded($rule, $options)) {
-        return [];
+    // Apply user func to $rule if available
+    if (isset($options['ruleFuncMap'][$group['QB']][$rule['field']])) {
+        $userFunc = $options['ruleFuncMap'][$group['QB']][$rule['field']];
+        return $userFunc($group, $rule, $options);
     }
-
-    $QB = isset($group['QB']) ? $group['QB'] : '';
-    if (isset($options['ruleFuncMap'][$QB][$rule['field']])) {
-        $userFunc = $options['ruleFuncMap'][$QB][$rule['field']];
-        // TODO: Change signature of $userFunc to take $group as well...
-        return ES\getFragment($group, $userFunc($rule));
-    }
-    return ES\getFragment($group, $rule);
+    return $rule;
 }
 
-function isRuleExcluded($rule, $options)
+function isRuleExcluded($group, $rule, $options)
 {
     if (isset($options['includeFields']) &&
         !in_array($rule['field'], $options['includeFields'], true)
     ) {
         return true;
     }
-
     if (isset($options['excludeFields']) &&
         in_array($rule['field'], $options['excludeFields'], true)
     ) {
         return true;
     }
-
     if (isset($options['excludeOperators']) &&
         in_array($rule['operator'], $options['excludeOperators'], true)
     ) {
         return true;
     }
-
     return false;
+}
+
+function transform($group, $options = [])
+{
+    return transformGroup($group, $options, DEFAULT_QB);
+}
+
+function transformGroup($group, $options, $context)
+{
+    $group = handleGroup($group, $options, $context);
+    if (empty($group['rules'])) {
+        return [];
+    }
+    return transformRules($group, $group['rules'], $options, $group['QB']);
+}
+
+function transformRule($group, $rule, $options, $context)
+{
+    if (isset($rule['rules'])) {
+        if ($context !== DEFAULT_QB) {
+            return transformGroup($rule, $options, $context);
+        }
+        return transformGroup($rule, $options, $group['QB']);
+    }
+    if (isRuleExcluded($group, $rule, $options)) {
+        return [];
+    }
+    return ES\getQuery($group, $rule);
+}
+
+function transformRules($group, $rules, $options, $context)
+{
+    $clauses = [];
+    foreach ($rules as $rule) {
+        $rule = handleRule($group, $rule, $options);
+        $query = transformRule($group, $rule, $options, $context);
+        if (empty($query)) {
+            continue;
+        }
+        $clause = ES\getClause($group, $rule);
+        if (!isset($clauses[$clause])) {
+            $clauses[$clause] = [];
+        }
+        $clauses[$clause][] = $query;
+    }
+    return ES\getCompoundQuery($clauses);
 }
